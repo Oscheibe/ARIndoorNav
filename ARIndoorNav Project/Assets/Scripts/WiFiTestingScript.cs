fa412990e9e6fe3b0ed350a655f542f1a971e78e
Oscheibe <contact.oliver.scheibert@gmail.com>
2019-05-17 13:54:29 +0200

    Reorganize Scripts and adding WiFiTestingScript
    The WiFi testing script will be used to test accesspoint distances

---
+++ ARIndoorNav Project/Assets/Scripts/WiFiTestingScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Android.Net.Wifi;
+
+public class WiFiTestingScript : MonoBehaviour
+{
+
+
+
+/**
+    Source: https://stackoverflow.com/questions/39566989/how-to-get-bssid-of-wifi-im-connecting-to-in-unity-c
+ */
+    public static string getBSSID()
+    {
+        string tempBSSID = "";
+
+        try
+        {
+            using (var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                using (var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi"))
+                {
+                    tempBSSID = wifiManager.Call<AndroidJavaObject>("getConnectionInfo").Call<string>("getBSSID");
+                }
+            }
+        }
+        catch(AndroidJavaException e)
+        {
+            
+        }
+        
+        return tempBSSID;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
