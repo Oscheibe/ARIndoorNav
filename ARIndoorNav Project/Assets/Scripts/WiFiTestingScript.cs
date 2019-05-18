fbe8c7af76d2d7bc57e385b86517ace6c260edcd
Oscheibe <contact.oliver.scheibert@gmail.com>
2019-05-18 18:48:59 +0200

    Getting Android permissions and requesting SSID
    It seems since Android 9, it will not be possible to do an accesspoint scan due to privacy reasons.

--- ARIndoorNav Project/Assets/Scripts/WiFiTestingScript.cs
+++ ARIndoorNav Project/Assets/Scripts/WiFiTestingScript.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using Android.Net.Wifi;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
 
 public class WiFiTestingScript : MonoBehaviour
 {
@@ -10,12 +12,15 @@ public class WiFiTestingScript : MonoBehaviour
 
     public Text debugText;
 
+    private string result;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        string result = "Debug Beginning";
-
+        
+        getLocationPermission();
+        
         result = getBSSID();
         Debug.Log(result);
         debugText.text = result;
@@ -28,27 +33,30 @@ public class WiFiTestingScript : MonoBehaviour
     }
 
 
+    private void getLocationPermission()
+    {
+        #if PLATFORM_ANDROID
+        if(!Permission.HasUserAuthorizedPermission("android.permission.ACCESS_WIFI_STATE"))
+        {
+            Permission.RequestUserPermission("android.permission.ACCESS_WIFI_STATE");
+            
+        }  
+        #endif
+    }
+
     /**
     Source: https://stackoverflow.com/questions/39566989/how-to-get-bssid-of-wifi-im-connecting-to-in-unity-c
     */
-    public static string getBSSID()
+    private string getBSSID()
     {
         string tempBSSID = "";
 
-        try
-        {
-            using (var activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
-            {
-                using (var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi"))
-                {
-                    tempBSSID = wifiManager.Call<AndroidJavaObject>("getConnectionInfo").Call<string>("getBSSID");
-                }
-            }
-        }
-        catch(AndroidJavaException e)
-        {
-            
-        }
+        AndroidJavaClass javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+        AndroidJavaObject activity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi");
+        AndroidJavaObject wifiReceiver = activity.Call<AndroidJavaObject>("new BroadcastReceiver()");
+
+        tempBSSID = wifiManager.Call<AndroidJavaObject>("getConnectionInfo").Call<string>("getSSID");
         
         return tempBSSID;
     }
