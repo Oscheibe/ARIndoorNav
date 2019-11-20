9db338a118aa5be5aec2023833cce6eefe23bdfd
Oscheibe <contact.oliver.scheibert@gmail.com>
2019-11-20 17:15:14 +0100

    Finished UI Prototyping
    The Prototypes to switch between menues and dispaly a list of buttons is now complete. It can be incorporated with the actual design in the future;

---
+++ ARIndoorNav Project/Assets/Scripts/Rapid Prototyping/ButtonListButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonListButton : MonoBehaviour
+{
+    public Text myText;
+    public Text destinationText;
+   
+    public void SetText(string textString)
+    {
+        myText.text = textString;
+    }
+
+    public void OnClick()
+    {
+        destinationText.text = myText.text;
+    }
+}
---
+++ ARIndoorNav Project/Assets/Scripts/Rapid Prototyping/UIMenuSwitchingManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIMenuSwitchingManager : MonoBehaviour
+{
+    public RectTransform mainMenu, settingsMenu;
+
+    private float animationSpeed = 0.25f; // in seconds
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, -535), animationSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenSettingsMenu()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-1200, -535), animationSpeed);
+        settingsMenu.DOAnchorPos(new Vector2(0, -535), animationSpeed);
+    }
+
+    public void CloseSettingsMenu()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, -535), animationSpeed);
+        settingsMenu.DOAnchorPos(new Vector2(1200, -535), animationSpeed);
+    }
+}
