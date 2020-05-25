2c6912d8e2ffb298d0415ee5784e6242a0b12da9
Oscheibe <contact.oliver.scheibert@gmail.com>
2020-05-25 16:26:28 +0200

    Remove unneccesary public statements

--- ARIndoorNav Project/Assets/Scripts/Model/Datastructures/IARVisuals.cs
+++ ARIndoorNav Project/Assets/Scripts/Model/Datastructures/IARVisuals.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * An interface for AR navigation visuals. 
+ * The information required for the navigation is: the path, the user's position
+ */
 public interface IARVisuals
 {
     
@@ -9,13 +13,13 @@ public interface IARVisuals
      * Gets called each update by NavigationPresenter
      * Starts displaying AR content
      */
-    public void SendNavigationPath(Vector3[] path);
+    void SendNavigationPath(Vector3[] path);
 
     /**
      * Clears current ARDisplay GameObjects
      * Will be overritten by the next SendNavigationPath() call
      */
-    public void ClearARDisplay();
+    void ClearARDisplay();
 
 
 }
